bba7cb71dd7d2697d938e53572487bee2979ab13
Bii <canaldesafios.1001@gmail.com>
2024-05-25 01:12:05 -0300

    att (tenho q arrumar mais algumas coisas)

--- site/pages/login.php
+++ site/pages/login.php
@@ -10,17 +10,20 @@ require_once '../banco/create_default.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/login.css">
     <title>Login</title>
 </head>
 <body>
-    <h1>Login</h1>
-    <form action="../banco/loginhandler.php" method="post">
-        <label for="user">Usuário</label>
-        <input type="text" name="user" placeholder="Usuário">
-        <label for="senha">Senha</label>
-        <input type="password" name="senha" placeholder="Senha">
-        <button>Entrar</button>
-    </form>
+    <div class="page">
+        <h1>Fazer Login</h1>
+        <form class="forms" action="../banco/loginhandler.php" method="post">
+            <label for="user">Usuário</label>
+            <input type="text" name="user" placeholder="Usuário">
+            <label for="senha">Senha</label>
+            <input type="password" name="senha" placeholder="Senha">
+            <button id="btnadm">Entrar</button>
+        </form>
+    </div>
 
     <?php
     check_erros_login();
