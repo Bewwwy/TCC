232a9758e50ef5ce3a5c0e380dfdb49da3e9483d
Elly <adryelly.oliveira@etec.sp.gov.br>
2024-09-15 21:05:49 -0300

    css excluir

--- site/banco/adocao/delete/deletehandler.php
+++ site/banco/adocao/delete/deletehandler.php
@@ -1,30 +1,46 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../css/excl.css">
+    <title>Excluir ADM</title>
+</head>
+<body>
+    <div class="q">
+        <?php
 
-$id = $_GET['id'];
+        $id = $_GET['id'];
 
-try {
-    require_once "../../conexao.php";
+        try {
+            require_once "../../conexao.php";
 
-    echo "<p>Tem certeza de que deseja excluir este pedido?</p>";
-    echo "<p>ID do pedido = ". $id ." </p>";
-    echo "<form method='post'>";
-    echo "<button type='submit' name='excl'>Excluir</button>";
-    echo "<button type='submit' name='cancel'>Cancelar</button>";
-    echo "</form>";
+            echo "<p>Tem certeza de que deseja excluir este pedido?</p>";
+            echo "<p>ID do pedido = ". $id ." </p>";
+            echo "<form method='post'>";
+            echo "<div class='btns'><button type='submit' name='excl' id='excl'>Excluir</button>";
+            echo "<button type='submit' name='cancel' id='canc'>Cancelar</button></div>";
+            echo "</form></div>";
 
 
-    if(isset($_POST['excl'])) {
-        $query = "DELETE FROM tb_f_adocao WHERE ID_form=" . $id .";";
+            if(isset($_POST['excl'])) {
+                $query = "DELETE FROM tb_f_adocao WHERE ID_form=" . $id .";";
 
-        $stmt = $pdo->prepare($query);
-        $stmt->execute();
-        echo "Exlcuido com sucesso.";
-        echo "<a href='../../../pages/p_adocao.php'>Voltar</a>";
+                $stmt = $pdo->prepare($query);
+                $stmt->execute();
+                echo "Exlcuido com sucesso.";
+                echo "<a href='../../../pages/p_adocao.php'>Voltar</a>";
 
-    } elseif (isset($_POST['cancel'])) {
-        header("Location: ../../../pages/p_adocao.php");
-    }
+            } elseif (isset($_POST['cancel'])) {
+                header("Location: ../../../pages/p_adocao.php");
+            }
 
-} catch (PDOException $e) {
-    die("Query failed; " . $e->getMessage());
-}
+        } catch (PDOException $e) {
+            die("Query failed; " . $e->getMessage());
+        }
+        ?>
+
+    
+</body>
+</html>
--- site/banco/animal/delete/deletehandler.php
+++ site/banco/animal/delete/deletehandler.php
@@ -1,30 +1,47 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../css/excl.css">
+    <title>Excluir ADM</title>
+</head>
+<body>
+    <div class="q">
+        <?php
 
-$id = $_GET['id'];
+        $id = $_GET['id'];
 
-try {
-    require_once "../../conexao.php";
+        try {
+            require_once "../../conexao.php";
 
-    echo "<p>Tem certeza de que deseja excluir este animal?</p>";
-    echo "<p>ID do animal = ". $id ." </p>";
-    echo "<form method='post'>";
-    echo "<button type='submit' name='excl'>Excluir</button>";
-    echo "<button type='submit' name='cancel'>Cancelar</button>";
-    echo "</form>";
+            echo "<p>Tem certeza de que deseja excluir este animal?</p>";
+            echo "<p>ID do animal = ". $id ." </p>";
+            echo "<form method='post'>";
+            echo "<div class='btns'><button type='submit' name='excl' id='excl'>Excluir</button>";
+            echo "<button type='submit' name='cancel' id='canc'>Cancelar</button></div>";
+            echo "</form></div>";
 
 
-    if(isset($_POST['excl'])) {
-        $query = "DELETE FROM tb_animal WHERE ID_pet=" . $id .";";
+            if(isset($_POST['excl'])) {
+                $query = "DELETE FROM tb_animal WHERE ID_pet=" . $id .";";
 
-        $stmt = $pdo->prepare($query);
-        $stmt->execute();
-        echo "Exlcuido com sucesso.";
-        echo "<a href='../../../pages/CRUDanimais.php'>Voltar</a>";
+                $stmt = $pdo->prepare($query);
+                $stmt->execute();
+                echo "Exlcuido com sucesso.";
+                echo "<a href='../../../pages/CRUDanimais.php'>Voltar</a>";
 
-    } elseif (isset($_POST['cancel'])) {
-        header("Location: ../../../pages/CRUDanimais.php");
-    }
+            } elseif (isset($_POST['cancel'])) {
+                header("Location: ../../../pages/CRUDanimais.php");
+            }
 
-} catch (PDOException $e) {
-    die("Query failed; " . $e->getMessage());
-}
+        } catch (PDOException $e) {
+            die("Query failed; " . $e->getMessage());
+        }
+                
+        ?>
+
+
+</body>
+</html>
